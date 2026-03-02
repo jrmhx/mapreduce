ce692d0036c2584fb1fc107e11f3f55d42a8ba7f
Jeremiah Xing <dev.jrmh@gmail.com>
2026-03-02 16:11:23 +1100

    update seq mapreduce

---
+++ cmd/coordinator/main.go
@@ -0,0 +1,32 @@
+package coordinator
+
+//
+// start the coordinator process, which is implemented
+// in ../mr/coordinator.go
+//
+// go run mrcoordinator.go pg*.txt
+//
+// Please do not change this file.
+//
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	mr "github.com/jrmhx/mapreduce"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "Usage: mrcoordinator sockname inputfiles...\n")
+		os.Exit(1)
+	}
+
+	m := mr.MakeCoordinator(os.Args[1], os.Args[2:], 10)
+	for m.Done() == false {
+		time.Sleep(time.Second)
+	}
+
+	time.Sleep(time.Second)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/jrmhx/mapReduce
+module github.com/jrmhx/mapreduce
 
 go 1.25.1
